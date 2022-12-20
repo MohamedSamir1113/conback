7e587064ae1c676a221a905501c2e111cee5b927
Hussein <94644643+Hussein-Elshinnawy@users.noreply.github.com>
2022-12-20 23:08:22 +0200

    Update AdminController.java

--- src/main/java/com/example/toolsprojectphase1/controller/AdminController.java
+++ src/main/java/com/example/toolsprojectphase1/controller/AdminController.java
@@ -11,7 +11,7 @@ import javax.websocket.server.PathParam;
 import java.util.List;
 @RestController
 @RequestMapping("/admin")
-@CrossOrigin(allowedHeaders = "", origins = "")
+@CrossOrigin(allowedHeaders = "*", origins = "*")
 
 public class AdminController {
     @Autowired
